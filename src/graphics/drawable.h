b6903758c76e188d7fda89a27d63ecf60fe488dd
liag <liag247@gmail.com>
2011-03-05 18:38:39 +0100

    Added a Drawable class with z level.

---
+++ src/graphics/drawable.h
@@ -0,0 +1,20 @@
+/*
+/ Drawable class with z value.
+/ Author: Victor Rådmark
+/ File created: 2011-03-04
+/ File updated: 2011-03-04
+/ License: GPLv3
+*/
+#ifndef DRAWABLE_H_INCLUDED
+#define DRAWABLE_H_INCLUDED
+
+#include <SFML/Graphics.hpp>
+
+namespace sbe
+{
+    class Drawable : sf::Drawable
+    {
+        private:
+            int z;
+    };
+}
